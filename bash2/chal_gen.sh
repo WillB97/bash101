6f2daad890e90830803fc185949f595b2225a433
WillB97 <will@rudi.org.uk>
2018-10-10 00:44:59 +0100

    fixed permissions

--- bash2/chal_gen.sh
+++ bash2/chal_gen.sh
@@ -5,4 +5,5 @@ for i in $(printf '%s\n' ${dirs[@]} | shuf ); do
 	cat /dev/urandom | tr -d -c '[:alpha:]'|head -c $(./random.sh 500 2000) |fold -w10 > $i/inhere
 done
 
+rm inhere
 echo "flag{$1}" | cat - /dev/urandom | tr -d -c '[:alpha:]{}\n-'|head -c 1033 |fold -w40 |head -c 1033 > $(printf '%s\n' ${dirs[@]} | shuf | head -n1)/inhere
--- bash3/chal_gen.sh
+++ bash3/chal_gen.sh
@@ -5,6 +5,7 @@ for i in $(printf '%s\n' ${dirs[@]} | shuf ); do
 	cat /dev/urandom | tr -d -c '[:alpha:]'|head -c $(./random.sh 500 2000) |fold -w10 > $i/inhere
 done
 
+rm inhere
 flag_file="$(printf '%s\n' ${dirs[@]} | shuf | head -n1)/inhere"
 echo "flag{$1}" | cat - /dev/urandom | tr -d -c '[:alpha:]{}\n-'|head -c $(./random.sh 500 2000) |fold -w40 > $flag_file
 chown bash0:bash1 $flag_file
